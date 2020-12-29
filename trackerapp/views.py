585bde4aae2b27071bec475a78a891c9f0a4f20a
Simone Abbamonte <simone@Simones-MacBook-Air.local>
2020-12-29 10:59:02 -0500

    added google maps API

--- trackerapp/views.py
+++ trackerapp/views.py
@@ -4,34 +4,36 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import New_Post
 
+
 def index(request):
+    # Show all posts
     if request.method == "GET":
         all_posts = New_Post.objects.all()
         return render(request, "trackerapp/index.html", {"all_posts": all_posts})
 
     else:
-        if not request.POST.get('diagnosis'):
-            print(request.POST.get('diagnosis'))
-            return render(request, "trackerapp/filter.html", {"message": "Please choose a diagnosis"})
+        # Create new post and error check for blanks
+        new_post = New_Post()
         if not request.POST['city']:
             return render(request, "trackerapp/filter.html", {"message": "Please type in a city"})
+        new_post.city = request.POST['city'].title()
+        if request.POST['state'] == "Choose...":
+            return render(request, "trackerapp/filter.html", {"message": "Please choose a state"})
+        new_post.state = request.POST['state']
         if not request.POST['zip']:
             return render(request, "trackerapp/filter.html", {"message": "Please type in a zip code"})
-        if not request.POST['state']:
-            return render(request, "trackerapp/filter.html", {"message": "Please choose a state"})
-
-        new_post = New_Post()
+        new_post.zip = request.POST['zip']
+        if not request.POST.get('diagnosis'):
+            return render(request, "trackerapp/filter.html", {"message": "Please choose a diagnosis"})
+        new_post.diagnosis = request.POST['diagnosis'] 
         new_post.name = request.POST['name']
         new_post.post = request.POST['post']
-        new_post.diagnosis = request.POST['diagnosis']  
-        new_post.city = request.POST['city']
-        new_post.zip = request.POST['zip']
-        new_post.state = request.POST['state']
         new_post.save()
-        return HttpResponseRedirect(reverse("index"))
-        
+        return HttpResponseRedirect(reverse("index"))  
 
 def filter(request):
+
+    # Show all posts in a list depending on the city and state entered by user
     if request.method == "GET":
         search = request.GET['city']
         if not search:
@@ -40,6 +42,7 @@ def filter(request):
         if not state or state == "Choose State...":
             return render(request, "trackerapp/filter.html", {"message": "Please choose a state"})
 
+        # Check if state and city entered match database and save in a list
         all_cities = New_Post.objects.all()
         filter_list = []
         for city in all_cities:
